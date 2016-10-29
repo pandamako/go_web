387eb0e514968c15c8a081187e8e4a4b29319ae5
Artem Bykov <artem.bikov@gmail.com>
2016-10-29 19:55:08 +0300

    remove pid file

--- web.go
+++ web.go
@@ -55,59 +55,17 @@ func fetch_redirect_url(form url.Values) (string, error) {
   return redirect_url.String(), nil
 }
 
-func write_pid() {
-  if *pid_path == "" {
-    return
-  }
-  f, err := os.Create(*pid_path)
-  if err != nil {
-    log.Fatal(err)
-  }
-  _, err = f.WriteString(fmt.Sprintf("%d", syscall.Getpid()))
-  f.Close()
-  if err != nil {
-    log.Fatal(err)
-  }
-}
-
-func is_file_exist() bool {
-  if *pid_path == "" {
-    return false
-  }
-  f, err := os.Open(*pid_path)
-  if err == nil {
-    f.Close()
-  }
-  return err == nil
-}
-
-func clear_pid() {
-  if *pid_path != "" {
-    os.Remove(*pid_path)
-  }
-}
-
-var pid_path = flag.String("p", "", "pid file")
-
 func main() {
   flag.Parse()
 
-  // if is_file_exist() {
-    // log.Fatal("already running")
-  // }
   mux := mux.NewRouter()
   mux.HandleFunc("/", root_handler).Methods("GET")
   mux.HandleFunc("/clicks", click_handler).Methods("GET")
 
   server := endless.NewServer(":8080", mux)
-  server.BeforeBegin = func(add string) {
-    write_pid()
-    log.Println("pid changed")
-  }
   err := server.ListenAndServe()
   if err != nil {
     log.Println(err)
   }
-  clear_pid()
   log.Println("Server on 4242 stopped")
 }
